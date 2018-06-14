378ec155707cb135cad892c7c62262728f29a274
wesley9mm <info.developer4@gmail.com>
2018-06-14 00:23:24 -0700

    game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,43 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+// 1. Wins: (# of times the user has guessed the letter correctly)
+ var wins = 0;       
+ // 2. Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
+ var losses = 0;
+// 3. Guesses Left: (# of guesses left. This will update)
+ var guessesLeft = 6;
+ var guessChoices = [];
+// 4. Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
+        
+// 5. When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
+        
+// 6. When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins). -->
+
+______________________________________________________________________________________________________
+
+// beginning: when the page loads, or when the game ends
+// and goes to the next round
+// what are your global variables?
+// meaning what variables need to persist throughout the game?
+// ex. win/loss record, number of guesses, your available
+// game choices. a, b, c, d, etc.
+// what are my events?
+// what happens during the event?
+// conditionals in event that shape if the game continues,
+// if the user loses, and if the user wins, and 
+// determining these things. 
+// When the user loses what happens?
+// When the user wins what happens?
+// var reset = function() {}
+
+
+
+
+document.onkeyup = function (event) {
+    
+}
+
 var userText = document.getElementById("Wins:");
+var userText = document.getElementById("Losses:");
+var userText = document.getElementById("Guesses:");
+
